b04f1a0d1d0d471a7cff58f97c0c93ada12ce8c4
ilhamymaged <elhamymaged500@gmail.com>
2025-12-01 22:44:58 +0200

    initial Setup

---
+++ Build/CMakeFiles/CMakeTarget.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CMakeTarget.dir/Vendor/glad/src/glad.c.o"
+  "CMakeFiles/CMakeTarget.dir/Vendor/glad/src/glad.c.o.d"
+  "CMakeFiles/CMakeTarget.dir/Vendor/stb/stb_image.c.o"
+  "CMakeFiles/CMakeTarget.dir/Vendor/stb/stb_image.c.o.d"
+  "CMakeFiles/CMakeTarget.dir/Vendor/tiny_obj_loader/tiny_obj_loader.cpp.o"
+  "CMakeFiles/CMakeTarget.dir/Vendor/tiny_obj_loader/tiny_obj_loader.cpp.o.d"
+  "CMakeFiles/CMakeTarget.dir/main.c.o"
+  "CMakeFiles/CMakeTarget.dir/main.c.o.d"
+  "CMakeTarget"
+  "CMakeTarget.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/CMakeTarget.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Build/Vendor/glfw/src/CMakeFiles/glfw.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/context.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/context.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/context.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/egl_context.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/egl_context.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/glx_context.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/glx_context.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/glx_context.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/init.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/init.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/init.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/input.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/input.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/input.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/linux_joystick.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/linux_joystick.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/linux_joystick.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/monitor.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/monitor.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/monitor.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/null_init.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/null_init.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/null_init.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/null_joystick.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/null_joystick.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/null_joystick.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/null_monitor.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/null_monitor.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/null_monitor.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/null_window.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/null_window.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/null_window.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/osmesa_context.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/osmesa_context.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/platform.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/platform.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/platform.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/posix_module.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/posix_module.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/posix_module.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/posix_poll.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/posix_poll.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/posix_poll.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/posix_thread.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/posix_thread.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/posix_time.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/posix_time.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/posix_time.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/vulkan.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/vulkan.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/window.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/window.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/window.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/wl_init.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/wl_init.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/wl_init.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/wl_monitor.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/wl_monitor.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/wl_monitor.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/wl_window.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/wl_window.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/wl_window.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/x11_init.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/x11_init.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/x11_init.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/x11_monitor.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/x11_monitor.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/x11_monitor.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/x11_window.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/x11_window.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/x11_window.c.o.d"
+  "/home/ilhamymaged/Dev/GameDevelopment/GameEngine/Vendor/glfw/src/xkb_unicode.c" "Vendor/glfw/src/CMakeFiles/glfw.dir/xkb_unicode.c.o" "gcc" "Vendor/glfw/src/CMakeFiles/glfw.dir/xkb_unicode.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
